b9882c29e71339dbd2a8c706a3ba8c88ff75fbe8
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-11 22:51:26 +0200

    errrrrrrrrrrr

--- functions/radio-browser.js
+++ functions/radio-browser.js
@@ -11,13 +11,15 @@ exports.getStation = async (stationName) => {
     "utf8",
     function readFileCallback(err, data) {
       if (err) {
-        console.log(err);
+        console.error(err);
       } else {
         let obj = JSON.parse(data); //now it an object
         obj.name = radio.body[0].name; //add some data
         obj.stream = radio.body[0].url_resolved; //add some data
         let json = JSON.stringify(obj); //convert it back to json
-        fs.writeFile(`${__dirname}/radio.json`, json, "utf8"); // write it back
+        fs.writeFile(`${__dirname}/radio.json`, json, "utf8", (result) => {
+          if (result) console.error(err);
+        }); // write it back
       }
     }
   );
