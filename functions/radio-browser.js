566a8b24216b4bd216fcba28dbe56e000f8462d5
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-11 22:47:51 +0200

    Update radio-browser.js

--- functions/radio-browser.js
+++ functions/radio-browser.js
@@ -6,15 +6,19 @@ exports.getStation = async (stationName) => {
     `http://91.132.145.114/json/stations/byname/${stationName}`
   );
 
-  fs.readFile("./radio.json", "utf8", function readFileCallback(err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      obj = JSON.parse(data); //now it an object
-      obj.name = data.body[0].name; //add some data
-      obj.stream = data.body[0].url_resolved; //add some data
-      json = JSON.stringify(obj); //convert it back to json
-      fs.writeFile("./radio.json", json, "utf8", callback); // write it back
+  fs.readFile(
+    `${__dirname}/radio.json`,
+    "utf8",
+    function readFileCallback(err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        obj = JSON.parse(data); //now it an object
+        obj.name = data.body[0].name; //add some data
+        obj.stream = data.body[0].url_resolved; //add some data
+        json = JSON.stringify(obj); //convert it back to json
+        fs.writeFile(`${__dirname}/radio.json`, json, "utf8", callback); // write it back
+      }
     }
-  });
+  );
 };
