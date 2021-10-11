5e65bbdcf4c292dd2fb0eb8fbc5379abff1bae8a
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-11 22:11:25 +0200

    Update Settings.jsx

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -5,27 +5,27 @@ const radiobrowser = require("../functions/radio-browser.js");
 module.exports = class vzradiosettings extends React.PureComponent {
   constructor(props) {
     super(props);
-
-    this.station = radiobrowser.getStationName(
-      getSetting("vz-radio-station", "Dash Pop X")
-    );
   }
 
   render() {
     const { getSetting, toggleSetting, updateSetting } = this.props;
 
-    return (
-      <>
-        <TextInput
-          note="Here you can enter any known radio station."
-          defaultValue={getSetting("vz-radio-station", "Dash Pop X")}
-          required={false}
-          onChange={(val) => updateSetting("vz-radio-station", val)}
-        >
-          Enter a radio station of your choice here
-        </TextInput>
-        Currently using <b>{station}</b> as your radio station!
-      </>
-    );
+    radiobrowser
+      .getStationName(getSetting("vz-radio-station", "Dash Pop X"))
+      .then((station) => {
+        return (
+          <>
+            <TextInput
+              note="Here you can enter any known radio station."
+              defaultValue={getSetting("vz-radio-station", "Dash Pop X")}
+              required={false}
+              onChange={(val) => updateSetting("vz-radio-station", val)}
+            >
+              Enter a radio station of your choice here
+            </TextInput>
+            Currently using <b>{station}</b> as your radio station!
+          </>
+        );
+      });
   }
 };
