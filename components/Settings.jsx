1d802396e645a6891f590a2d69122b72a44ea036
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-11 22:09:15 +0200

    Update Settings.jsx

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -3,12 +3,14 @@ const { TextInput } = require("@vizality/components/settings");
 const radiobrowser = require("../functions/radio-browser.js");
 
 module.exports = class vzradiosettings extends React.PureComponent {
-  async constructor(props) {
+  constructor(props) {
     super(props);
-    this.station = await radiobrowser.getStationName(
-      await getSetting("vz-radio-station", "Dash Pop X")
-    );
   }
+
+  station = await radiobrowser.getStationName(
+    await getSetting("vz-radio-station", "Dash Pop X")
+  );
+
   render() {
     const { getSetting, toggleSetting, updateSetting } = this.props;
 
@@ -22,7 +24,7 @@ module.exports = class vzradiosettings extends React.PureComponent {
         >
           Enter a radio station of your choice here
         </TextInput>
-        Currently using <b>{this.station}</b> as your radio station!
+        Currently using <b>{station}</b> as your radio station!
       </>
     );
   }
