e58a246c0ee31556478ba3a20ac1eba2c098fb41
bakzkndd <50497725+bakzkndd@users.noreply.github.com>
2021-10-13 21:27:14 +0200

    fix the slider

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,7 +1,8 @@
 const { React } = require("@vizality/webpack");
 const { TextInput, SliderInput } = require("@vizality/components/settings");
 const radiobrowser = require("../functions/radio-browser.js");
-const audio = require('../functions/audio')
+const audio = require("../functions/audio");
+const radio = require("../functions/radio.json");
 let cooldown = false;
 
 module.exports = class vzradiosettings extends React.PureComponent {
@@ -22,7 +23,11 @@ module.exports = class vzradiosettings extends React.PureComponent {
           onChange={(val) => {
             updateSetting("vz-radio-station", val);
             if (!cooldown) {
-              radiobrowser.getStation(val, getSetting('volume-slider', 100), getSetting("vz-radio", false));
+              radiobrowser.getStation(
+                val,
+                getSetting("volume-slider", 100),
+                getSetting("vz-radio", false)
+              );
               cooldown = true;
               setTimeout(function () {
                 cooldown = false;
@@ -33,22 +38,21 @@ module.exports = class vzradiosettings extends React.PureComponent {
           Enter a radio station of your choice here
         </TextInput>
         Currently using <b>{radio.name}</b> as your radio station!
-
         <SliderInput
-        disabled={false}
-        note={'Volume slider.'}
-        initialValue={100}
-        defaultValue={getSetting('volume-slider', 100)}
-        onChange={v => {
-          updateSetting('volume-slider', v)
-          if(getSetting("vz-radio", false)) {
-            audio.stop()
-            audio.play(getSetting("vz-radio-station", "Dash Pop X"), v)
-          }
-        }}
+          disabled={false}
+          note={"Volume slider."}
+          initialValue={100}
+          defaultValue={getSetting("volume-slider", 100)}
+          onValueChange={(v) => {
+            updateSetting("volume-slider", v);
+            if (getSetting("vz-radio", false)) {
+              audio.stop();
+              audio.play(radio.stream, v);
+            }
+          }}
         >
           Change the volume here
-      </SliderInput>
+        </SliderInput>
       </>
     );
   }
